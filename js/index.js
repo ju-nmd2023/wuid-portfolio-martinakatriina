939cea3d7ca84d43d9e7e9e5f3e2ff6db545e1fe
martinakatriina <martinacitron@gmail.com>
2023-11-24 13:19:59 +0100

    validation form
    Added eventlistener

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-function validateForm() {
+/* function validateForm() {
   var firstName = document.getElementById("firstN");
   var secondName = document.getElementById("SecondN");
   var email = document.getElementById("Email");
   var subject = document.getElementById("Subject");
   var errorMessage = document.getElementById("error-message");
+
   if (
     firstName.value.trim() === "" ||
     secondName.value.trim() === "" ||
@@ -14,8 +15,39 @@ function validateForm() {
     return false; //
     /*  alert("No blank values allowed");
     return false; */
+/* } else {
+    alert("");
+    return true;
+  }
+} */
+
+// Retrieve the button element
+const buttonelement = document.getElementById("button");
+
+// Define the validateForm function separately
+function validateForm() {
+  var firstName = document.getElementById("firstN");
+  var secondName = document.getElementById("SecondN");
+  var email = document.getElementById("Email");
+  var subject = document.getElementById("Subject");
+  var errorMessage = document.getElementById("error-message");
+
+  if (
+    firstName.value.trim() === "" ||
+    secondName.value.trim() === "" ||
+    email.value.trim() === "" ||
+    subject.value.trim() === ""
+  ) {
+    alert("No blank values allowed");
+    return false;
   } else {
     alert("");
     return true;
   }
 }
+
+// Add event listener to the button
+buttonelement.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevents the default form submission
+  validateForm(); // Call the validateForm function when the button is clicked
+});
